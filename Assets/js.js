4118d0f259bfba97b52cac96a80fb48190b02837
Frank McDermott <fmcdermott4@gmail.com>
2021-03-20 00:22:08 -0400

    CSS fixed to allow user to input into table correctly

--- Assets/js.js
+++ Assets/js.js
@@ -2,37 +2,18 @@
 
 // calls updateDateAndTime every second to update header information
 updateDateAndTime();
-colorChange();
 setInterval( updateDateAndTime , 1000)
+colorChange();
+setInterval( colorChange , 1000)
 
 // Function for updating date and time in header
 function updateDateAndTime() {
-    var today = new Date();
-    var day = today.getDay();
-    var month = today.getMonth();
-    var year = today.getFullYear();
-    var days = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-    var months = ["January","February","March","April","May","June","July","August","September","October","November","November","December"]
     var currentDay = $("#currentDay");
     var currentTime = $("#currentTime");
     currentDay.innerHTML = "";
     currentTime.innerHtml = "";
-    currentDay.text("Today is " + days[day] + ", " + months[month] + " " + day + ", " + year +".");
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-    var seconds = today.getSeconds();
-    var zero;
-    var amPm = " AM"
-    if(hours >=12){
-        amPm = " PM";
-        hours -=12;
-    }
-    if (minutes < 10 ){
-        zero = "0";        
-    } else {
-        zero = "";
-    }
-    currentTime.text("Current time is " + hours + ":" + zero + minutes +" " + amPm)
+    currentDay.text("Today is " + moment().format('dddd') + ", " +moment().format('ll')) 
+    currentTime.text("Current time is " + moment().format('LT'))
 }
 // Changes color of table elements besed on time of day
 function colorChange () {
