6ca610b34b85ba947e2bebe3b0f39f7ad324490e
Frank McDermott <fmcdermott4@gmail.com>
2021-03-20 15:03:48 -0400

    JS functioning, refactoring

--- Assets/js.js
+++ Assets/js.js
@@ -12,17 +12,15 @@ function dayObject() {
     var today = moment().format('L');
     if (localStorage.getItem(today) === null){
         var plan = {};
-        for (i = 0; i<9; i++) {
-            plan[i] = "Nothing planned" + i;     
+        for (i = 0; i < 10; i++) {
+            plan[i] = ""     
         localStorage.setItem(today , JSON.stringify(plan));
         } 
     }
-    var form = document.querySelectorAll(".form-Control");
-    for (i=0 ; i<form.length;i++){
-        debugger;
-        form[i];
-        
-
+    var form = document.querySelectorAll(".form-control");
+    for (i=0 ; i<form.length ;i++){
+        form[i].value = JSON.parse(localStorage.getItem(today))[i];     
+    }
 }
 // Function for updating date and time in header
 function updateDateAndTime() {
@@ -53,11 +51,11 @@ function colorChange () {
     }
 }
 
-// function button(i){
-//     var times = document.querySelectorAll(".timeSlot");
-//     var today = moment().format('L');
-//     var day = localStorage.getItem(today)
-//     debugger;
-    // console.log(times[i].childNodes[0].nextSibling.nextElementSibling.childNodes[0].value)
-    // localStorage.setItem(day[i] , times[i].childNodes[0].nextSibling.nextElementSibling.childNodes[0].value);
-// }
+function button(i){
+    var times = document.querySelectorAll(".timeSlot");
+    var text = times[i].childNodes[0].nextSibling.nextElementSibling.childNodes[0].value
+    var today = moment().format('L');
+    var day = JSON.parse(localStorage.getItem(today));
+    day[i] = text;
+    localStorage.setItem(today , JSON.stringify(day));
+}
