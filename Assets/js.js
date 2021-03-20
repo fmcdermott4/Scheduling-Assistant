9fd79a5fe1f699e14d0877d0a5e9e5b8668e3b08
Frank McDermott <fmcdermott4@gmail.com>
2021-03-20 14:33:17 -0400

    button to save data in local storage almost done

--- Assets/js.js
+++ Assets/js.js
@@ -5,7 +5,25 @@ updateDateAndTime();
 setInterval( updateDateAndTime , 1000)
 colorChange();
 setInterval( colorChange , 1000)
+dayObject();
 
+// Declare day object if it doesn't exist
+function dayObject() {
+    var today = moment().format('L');
+    if (localStorage.getItem(today) === null){
+        var plan = {};
+        for (i = 0; i<9; i++) {
+            plan[i] = "Nothing planned" + i;     
+        localStorage.setItem(today , JSON.stringify(plan));
+        } 
+    }
+    var form = document.querySelectorAll(".form-Control");
+    for (i=0 ; i<form.length;i++){
+        debugger;
+        form[i];
+        
+
+}
 // Function for updating date and time in header
 function updateDateAndTime() {
     var currentDay = $("#currentDay");
@@ -15,13 +33,16 @@ function updateDateAndTime() {
     currentDay.text("Today is " + moment().format('dddd') + ", " +moment().format('ll')) 
     currentTime.text("Current time is " + moment().format('LT'))
 }
-// Changes color of table elements besed on time of day
+// Changes color of table elements based on time of day
 function colorChange () {
     var today = new Date();
     var hour = today.getHours();
     var times = document.querySelectorAll(".timeSlot");
     var timeSlots = [8,9,10,11,12,13,14,15,16,17]
     for (var i = 0; i < timeSlots.length; i++) {
+        times[i].classList.remove("past");
+        times[i].classList.remove("present");
+        times[i].classList.remove("future");
         if(hour > timeSlots[i]){
             times[i].classList.add("past");
         }else if (hour == timeSlots[i]) {
@@ -31,3 +52,12 @@ function colorChange () {
         }
     }
 }
+
+// function button(i){
+//     var times = document.querySelectorAll(".timeSlot");
+//     var today = moment().format('L');
+//     var day = localStorage.getItem(today)
+//     debugger;
+    // console.log(times[i].childNodes[0].nextSibling.nextElementSibling.childNodes[0].value)
+    // localStorage.setItem(day[i] , times[i].childNodes[0].nextSibling.nextElementSibling.childNodes[0].value);
+// }
